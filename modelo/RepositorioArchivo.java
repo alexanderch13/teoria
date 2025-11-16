76fc060c524eb4557338f3dee593090ec2109eae
alexanderch13 <alexander.chipana@estudiante.ucsm.edu.pe>
2025-11-16 12:09:25 -0500

    feat: Clase repositorioArchivo creado.

---
+++ modelo/RepositorioArchivo.java
@@ -0,0 +1,34 @@
+public class RepositorioArchivo extends RepositorioAsociativo<Equipo, Mantenimiento>
+{
+  private final String nombreArchivo;
+  
+  public RepositorioArchivo(String nombreArchivo)
+  {
+    this.nombreArchivo=nombreArchivo;
+  }
+
+  public void guardarEnArchivo() throws IOException
+  {
+    try (BufferedWriter bw = new BufferedWriter(new FileWriter(nombreArchivo)))
+    {
+      for (ParAsociativo<Equipo, Mantenimiento> par : asociaciones)
+      {
+        Equipo e = par.getPrimero();
+        Mantenimiento = par.getSegundo();
+        
+        String linea = String.join (",",
+          String.valueOf(e.getId()),
+          e.getNombre(),
+          e.getTipo(),
+          String.valueOf(m.getId()),
+          m.getDescripcion(),
+          m.getTecnico(),
+          String.valueOf(m.getFecha()),
+          String.valueOf(m.getCosto())
+          );
+        bw.write(linea);
+        bw.newLine();
+      }
+    }
+  }
+}
