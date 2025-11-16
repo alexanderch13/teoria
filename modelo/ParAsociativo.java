eaaa1fc97586394616d36df1646d2408485fba42
alexanderch13 <alexander.chipana@estudiante.ucsm.edu.pe>
2025-11-16 11:23:03 -0500

    feat: Clase ParAsociativo agregado.

---
+++ modelo/ParAsociativo.java
@@ -0,0 +1,27 @@
+public class ParAsociativo<T,U> implements Serializable
+{
+  private static final long serialVersionUID = 1L;
+  private T primero;
+  private U segundo;
+
+  public ParAsociativo(T primero, U segundo)
+  {
+    this.primero=primero;
+    this.segundo=segundo;
+  }
+
+  public T getPrimero()
+  {
+    return primero;
+  }
+  public U getSegundo()
+  {
+    return segundo;
+  }
+
+  @Override
+  public String toString()
+  {
+    return primero.toString() + "--->" + segundo.toString();
+  }
+}
