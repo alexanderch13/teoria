9751db4a7e44245e878467f416f10f8beb1c29d5
Lizbeth <pierinagdlv@gmail.com>
2025-11-16 13:38:42 -0500

    Update Mantenimiento.java

---
+++ modelo/Mantenimiento.java
@@ -0,0 +1,42 @@
+import java.time.LocalDate;
+
+public class Mantenimiento {
+
+    private int id;
+    private String descripcion;
+    private String tecnico;
+    private LocalDate fecha;
+    private double costo;
+
+    public Mantenimiento(int id, String descripcion, String tecnico, LocalDate fecha, double costo) {
+        this.id = id;
+        this.descripcion = descripcion;
+        this.tecnico = tecnico;
+        this.fecha = fecha;
+        this.costo = costo;
+    }
+
+    public int getId() { 
+        return id; 
+    }
+
+    public String getDescripcion() { 
+        return descripcion; 
+    }
+
+    public LocalDate getFecha() { 
+        return fecha; 
+    }
+
+    public String getTecnico() { 
+        return tecnico; 
+    }
+
+    public double getCosto() { 
+        return costo; 
+    }
+
+    public void setTecnico(String tecnico) {
+        this.tecnico = tecnico;
+    }
+}
