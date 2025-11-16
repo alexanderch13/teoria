b9ea51b90e17b4a87b6927c895922f2da9f3fb4d
alexanderch13 <alexander.chipana@estudiante.ucsm.edu.pe>
2025-11-16 11:15:50 -0500

    feat: Clase equipo agregada

--- modelo/Equipo.java
+++ modelo/Equipo.java
@@ -1,5 +1,32 @@
 public class Equipo
 {
   private int id;
+  private String nombre;
+  private String tipo;
 
+  public Equipo(int id,String nombre, String tipo)
+  {
+    this.id=id;
+    this.nombre=nombre;
+    this.tipo=tipo;
+  }
+
+  public int getId()
+  {
+    return id;
+  }
+  public String getNombre()
+  {
+    return nombre;
+  }
+  public String getTipo()
+  {
+    return tipo;
+  }
+
+  @Override
+  public String toString()
+  {
+    return String.format("%d - %s (%s) ", id,nombre,tipo);
+  }
 }
